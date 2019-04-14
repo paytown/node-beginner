4c21e2c693f81fe7c05671e4ae1a8d5c4aa6a444
Payton Lower <paytown@me.com>
2019-04-14 14:51:05 -0700

    non blocking operations

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,20 @@
 const exec = require('child_process').exec;
 
-function start() {
+function start(res) {
   console.log('Request handler \'start\' was called.');
-  let content = 'empty';
 
-  exec('ls -lah', function(error, stdout, stderr) {
-    content = stdout;
+  exec('ls -lah', (error, stdout, stderr) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write(stdout);
+    res.end();
   });
-
-  return content;
 }
 
-function upload() {
-  console.log('Request handler \'upload\' was called');
-  return 'Hello upload';
+function upload(res) {
+  console.log('Request handler \'upload\' was called.');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hello Upload');
+  res.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -6,13 +6,7 @@ function start(route, handle) {
     const pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' received');
 
-    // route(handle, pathname);
-
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-    const content = route(handle, pathname);
-    res.write(content);
-    res.end();
+    route(handle, pathname, res);
   }
 
   http.createServer(onRequest).listen(8888);
