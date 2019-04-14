a17e26bc12d9c7254872dd6f42bbb97c6a00efe3
Payton Lower <paytown@me.com>
2019-04-14 13:49:45 -0700

    server fn built

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 const http = require('http');
 
-// SERVER WITH CALLBACKS
-function onRequest(req, res) {
-  console.log('Request received');
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello again');
-  res.end();
+function start() {
+  function onRequest(req, res) {
+    console.log('Request received');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello world');
+    res.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Server started');
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server has started');
-
-
-// SIMPLE SERVER CODE
-// http.createServer((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-//   res.write('Hello');
-//   res.end();
-// }).listen(8888);
+exports.start = start;
