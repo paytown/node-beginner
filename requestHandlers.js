e3245195c28b8a40cc0235bccf7c09530822ca09
Payton Lower <paytown@me.com>
2019-04-14 15:04:17 -0700

    input form

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,11 +3,27 @@ const exec = require('child_process').exec;
 function start(res) {
   console.log('Request handler \'start\' was called.');
 
-  exec('ls -lah', (error, stdout, stderr) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write(stdout);
-    res.end();
-  });
+  const body = `
+  <html>
+    <head>
+      <meta http-equiv="Content-Type" content="text/html"
+      <title>File Upload</title>
+    </head>
+    <body>
+      <form action="/upload" method="post">
+        <textarea name="text" rows="20" cols="60"></textarea>
+        <input type="submit" value="submit text" />
+      </form>
+    </body>
+  </html>
+  `;
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(body);
+  res.end();
+  
+  // exec('ls -lah', (error, stdout, stderr) => {
+  // });
 }
 
 function upload(res) {
