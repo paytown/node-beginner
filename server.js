b1fa324d06adbc577b1552b9c4ee233b3fcb1e02
Payton Lower <paytown@me.com>
2019-04-14 14:26:40 -0700

    bad way to make req handlers respond

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ function start(route, handle) {
     const pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' received');
 
-    route(handle, pathname);
+    // route(handle, pathname);
 
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello world');
+
+    const content = route(handle, pathname);
+    res.write(content);
     res.end();
   }
 
