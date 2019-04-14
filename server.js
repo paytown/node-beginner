a89383414f309c43723bd58cbe3b5f6dce6def89
Payton Lower <paytown@me.com>
2019-04-13 17:31:14 -0700

    server updated with callbacks

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const http = require('http');
 
-http.createServer((req, res) => {
+function onRequest(req, res) {
+  console.log('Request received');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello');
+  res.write('Hello again');
   res.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('Server has started');
+
+
+// http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.write('Hello');
+//   res.end();
+// }).listen(8888);
